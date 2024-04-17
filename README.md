91a579103f19c15154760cd742c29c486615e888
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-04-18 00:17:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[Demo](https://agency-website-orpin.vercel.app/)
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link as ScrollLink } from "react-scroll";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
+
+    // Toggle menu
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsSticky(window.scrollY > 100);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    // Nav items array
+    const navItems = [
+        { link: "Home", path: "home" },
+        { link: "Service", path: "service" },
+        { link: "About", path: "about" },
+        { link: "Product", path: "product" },
+        { link: "Testimonial", path: "testimonial" },
+        { link: "FAQ", path: "faq" },
+    ];
+
+    return (
+        <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
+            <nav className={`py-4 lg:px-14 px-4  ${isSticky ? "sticky top-0 left-0 right-0 border bg-white duration-300" : ""}`}>
+                <div className="flex justify-between items-center text-base gap-8">
+                    <a href="/" className="text-2xl font-semibold flex items-center space-x-3">
+                        <img src={logo} className="w-10 inline-block items-center" alt="" />
+                        <span className="text-[#263238]">GREENWAY</span>
+                    </a>
+                    {/* Nav items for large screens */}
+                    <ul className="md:flex space-x-12 hidden">
+                        {navItems.map(({ link, path }) =>
+                            <ScrollLink key={path} to={path} spy={true} smooth={true} offset={-100} className="block text-base text-gray-900 hover:text-brandPrimary">{link}</ScrollLink>
+                        )}
+                    </ul>
+
+                    {/* btn for large device */}
+                    <div className="space-x-12 hidden lg:flex items-center">
+                        <a href="" className="hidden lg:flex items-center text-brandPrimary hover:text-gray-900">Login</a>
+                        <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-200 rounded hover:bg-neutralGrey">signUp</button>
+                    </div>
+                    {/* menu btn only for mobile */}
+                    <div className="md:hidden">
+                        <button
+                            onClick={toggleMenu}
+                            className="focus:outline-none focus:text-gray-500">
+                            {
+                                isMenuOpen ? (<FaTimes className="h-6 w-6 text-neutralDgrey" />) : (<FaBars className="h-6 w-6 text-neutralDgrey" />)
+                            }
+                        </button>
+                    </div>
+                </div>
+            </nav>
+            {/* nav items for mobile device */}
+            <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+                {navItems.map(({ link, path }) =>
+                    <ScrollLink key={path} to={path} spy={true} smooth={true} offset={-100} onClick={toggleMenu} className="block text-base text-white hover:text-brandPrimary first:font-medium">{link}</ScrollLink>
+                )}
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
